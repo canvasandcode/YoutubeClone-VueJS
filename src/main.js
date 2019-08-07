a683ceb971b14df01af702f769eb8083753ac2db
Imran Rahman <imran7147@gmail.com>
2019-08-07 13:23:55 +0100

    adding search bar component to project

--- src/main.js
+++ src/main.js
@@ -1,8 +1,7 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from  'vue';
+import App from './App';
 
-Vue.config.productionTip = false
-
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+//Same as above
+new Vue ({
+    render: h => h(App)
+}).$mount('#app');
