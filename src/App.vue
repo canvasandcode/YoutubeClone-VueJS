fbb547b69dd26077c5062c0fb81a58ee3b89a5ba
imran.rahman <imran7147@gmail.com>
2019-08-07 21:27:35 +0100

    updating Axios response to fill empty array

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,54 @@
 <template>
-    <div>
-        <!--listens for termChange event to be emitted from searchbar, then runs function-->
-        <SearchBar @termChange="onTermChange"></SearchBar>
-        <VideoList></VideoList>
-    </div>
+  <div>
+    <!--listens for termChange event to be emitted from searchbar, then runs function-->
+    <SearchBar @termChange="onTermChange"></SearchBar>
+    <VideoList></VideoList>
+    {{ videos.length }}
+    <!--retrieve length from videos data property-->
+  </div>
 </template>
 
 <script>
-import axios from 'axios';
-import SearchBar from './components/SearchBar';
-import VideoList from './components/VideoList';
+import axios from "axios";
+import SearchBar from "./components/SearchBar";
+import VideoList from "./components/VideoList";
 
-const API_KEY = 'AIzaSyCm3jr9B1jvJ87taITzUsHGXw9vEMKIAUw'; //using caps here as its const, to make more clear
+const API_KEY = "AIzaSyCm3jr9B1jvJ87taITzUsHGXw9vEMKIAUw"; //using caps here as its const, to make more clear
 
 export default {
-    //helps identify when debugging in browser
-    name: 'App', 
-    components: {
-        SearchBar,
-        VideoList
-    },
-    methods: {
-        //searchTerm is 2nd arg from searchbar.vue file, passed the users input through this
-        onTermChange(searchTerm) { 
-            //and log it here
-            // console.log(searchTerm); 
+  //helps identify when debugging in browser
+  name: "App",
+  components: {
+    SearchBar,
+    VideoList
+  },
+  data() {
+    //have to use a function that returns a data property as we are in a component
+    return { videos: [] };
+  },
+  methods: {
+    //searchTerm is 2nd arg from searchbar.vue file, passed the users input through this
+    onTermChange(searchTerm) {
+      //and log it here
+      // console.log(searchTerm);
 
-            axios.get('https://www.googleapis.com/youtube/v3/search', {
-                params: {
-                    key: API_KEY,
-                    type: 'video',
-                    part: 'snippet',
-                    q: searchTerm
-                }
-            }).then(response => console.log(response));
-        }
+      axios
+        .get("https://www.googleapis.com/youtube/v3/search", {
+          params: {
+            key: API_KEY,
+            type: "video",
+            part: "snippet",
+            q: searchTerm
+          }
+        })
+        .then(response => {
+          this.videos = response.data.items; //this is the data property that is tied to response from youtube, not our instance
+        });
     }
+  }
 };
 </script>
 
 <style>
-
 </style>
 
