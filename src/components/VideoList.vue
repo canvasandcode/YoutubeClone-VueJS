78ea060e3a290f3bf9fde1fc1c7e6a1421ac8bee
Imran Rahman <imran7147@gmail.com>
2019-08-09 14:46:16 +0100

    adding video list item component

--- src/components/VideoList.vue
+++ src/components/VideoList.vue
@@ -1,16 +1,26 @@
 <template>
     <ul>
-        VideoList
-        {{videos.length}}
+        <!--'video' acts as a temporary variable that is available only inside template, no property called video exists- video in bind speechmarks has to match name-->
+        <VideoListItem 
+            v-for="video in videos" 
+            :video="video"
+            :key="video.etag">
+        </VideoListItem>
+        {{videos.length}} <!--print out the number of videos that has been passed down as props from App.vue-->
     </ul>
 </template>
 
 <script>
+import VideoListItem from './VideoListItem';
+
 export default {
     name: 'VideoList',
+    components: {
+        VideoListItem
+    },
     props: {
         //'videos' has to be called the same as how it is in parent component
-        videos: Array // telling comp that will receive a prop called videos and expect it to be an array
+        videos: Array // telling component that it will receive a prop called 'videos' and TYPE has to be an Array otherwise will throw an error
     } 
 };
 </script>
---
+++ src/components/VideoListItem.vue
@@ -0,0 +1,14 @@
+<template>
+    <li>
+        {{video.snippet.title}} <!--name of video)-->
+    </li>
+</template>
+
+<script>
+export default {
+    name: 'VideoListItem',
+    props: ['video'] //expect to get a property from parent called video 
+}
+</script>
+
+<!--removed style tag as nothing will really be styled here-->
